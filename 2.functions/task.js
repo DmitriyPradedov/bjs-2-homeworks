ac2e37b973f6605fcaf6daebeba8872ec1a18a9f
DmitriyPradedov <pradedov72@mail.ru>
2022-07-15 17:11:41 +0700

    задача 1

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -2,10 +2,25 @@
 function getArrayParams(arr) {
   let min, max, sum, avg;
 
-  // Ваш код
+  min = arr[0];
+  max = arr[0];
+  sum = 0;
+  avg = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    } else if (arr[i] < min) {
+      min = arr[i];
+    }
+    sum += arr[i];
+   
+  }
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
+
 
 // Задание 2
 function worker(arr) {
