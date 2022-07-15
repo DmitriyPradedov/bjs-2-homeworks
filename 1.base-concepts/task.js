aa01c23b74f724d58fbd12a5949d52158ac4aae4
DmitriyPradedov <pradedov72@mail.ru>
2022-07-15 10:33:42 +0700

    изменение 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -7,7 +7,7 @@ function solveEquation(a, b, c) {
     let x1 = (-b + Math.sqrt(d))/(2*a);
     let x2 = (-b - Math.sqrt(d))/(2*a);
     arr.push(x1, x2);
-  } else if (d == 0) {
+  } else if (d === 0) {
       let x1 = -b/(2*a);
       arr.push(x1);
   }
