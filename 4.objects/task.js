aed148d87057f014f80f47fb1f0e6eaf6719d7b4
DmitriyPradedov <pradedov72@mail.ru>
2022-07-21 09:11:56 +0700

    задача объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
-function Student(name, gender, age) {
-    // Ваш код
 
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Mark", "male", 41);
+let student3 = new Student("Mary", "female", 33);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+ this.subject = subjectName;
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+  }
+};
+
+ Student.prototype.addMarks = function (...mark) {
+  if(this.marks === undefined){
+    this.marks = [...mark];
+    } else {
+      this.marks.push(...mark);
+    }
+ };
+
+ Student.prototype.getAverage = function () {
+   return this.marks.reduce((acc, mark, index) => {
+    acc +=mark;
+    if(index === this.marks.length - 1) {
+      return acc / this.marks.length;
+    }
+    return acc;
+   });
+ };
+
+ Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+  return this;
+ };
