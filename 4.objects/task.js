0a7dd17fef26209494713a309ad8fe9fd546fb12
DmitriyPradedov <pradedov72@mail.ru>
2022-07-21 09:42:52 +0700

    объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -41,7 +41,8 @@ Student.prototype.addMark = function (mark) {
   this.excluded = reason;
   return this;
  };
- let student1 = new Student("Tony", "male", 37);
+ 
+let student1 = new Student("Tony", "male", 37);
 student1.setSubject("Algebra");
 student1.addMark(5);
 student1.addMark(4);
@@ -54,5 +55,5 @@ student2.setSubject("Geometry");
 student2.addMark(2);
 student2.addMark(3);
 student2.addMark(2);
-student2.exclude('low grades')
+student2.exclude('low grades');
 console.log(student2);
