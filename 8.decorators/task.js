9486a0904a6b50c77781acbd00b9b0f3788aebba
DmitriyPradedov <pradedov72@mail.ru>
2022-08-11 15:52:32 +0700

    декораторы

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,63 @@
+//Задача 1. Усовершенствуйте кэширующий декоратор
+ 
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+  sizeCache = 5;
+ 
+  function wrapper(...args) {
+      const hash = args.join(',');
+ 
+      let value = cache.find((item) => item.hash === hash)
+      if (value != undefined) {
+          console.log("Из кеша: " + value.result);
+          return "Из кэша: " + value.result;
+      } else {
+          let result = func(...args);
+          cache.push({hash, result});
+          if (cache.length > sizeCache) {
+            cache.shift();
+          }
+          console.log("Вычисляем: " + result);
+          return "Вычисляем: " + result;
+      }
+  }
+  return wrapper;
 }
 
+//Задача 2. Фильтрация и преобразование массива
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(f, wait) {
+  let flag = false;
+
+  return function () {
+    if (!flag) {
+      f();
+      flag = true;
+      setTimeout(() => {
+        flag = false;
+        console.log('Флаг сброшен')}, wait);
+    } else {
+          console.log('проигнорировано');
+        }  
+  };
 }
 
-function debounceDecorator2(func) {
-  // Ваш код
+//Задача 3. Усовершенствуйте debounceDecoratorNew
+
+function debounceDecorator2(f, wait) {
+  let flag = false;
+  wrapper.count = 0;
+
+  function wrapper() {
+    if (!flag) {
+      wrapper.count++;
+      f();
+      flag = true;
+      setTimeout(() => flag = false, wait);
+    } else {
+          console.log('проигнорировано');
+        }  
+  };
+
+  return wrapper;
 }
